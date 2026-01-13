91e663da7485f1046e37954bdb996618af482a35
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-01-13 17:24:39 +0200

    Added GamesController with a view and connected it to the navigation bar of the app.

---
+++ GamingZoneApp/Controllers/GamesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamingZoneApp.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("All games.");
+        }
+
+        [Route("Games/GameDetails/{id}")]
+        public IActionResult GameDetails(int id)
+        {
+            string errorMessage = string.Empty;
+
+            if(id <= 0)
+            {
+                errorMessage = "Invalid game Id.";                
+            }
+            else
+            {
+                errorMessage = $"Game with Id: {id}";
+            }
+
+            return Ok(errorMessage);
+                
+        }
+    }
+}
