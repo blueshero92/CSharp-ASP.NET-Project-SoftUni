d5066920eb5a40fbf0c06b926e2f2900bfcf8c25
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-01-26 17:00:09 +0200

    Added controllers for Developer and Publisher.

---
+++ GamingZoneApp/Controllers/DevelopersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamingZoneApp.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GamingZoneApp/Controllers/PublishersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamingZoneApp.Controllers
+{
+    public class PublishersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
