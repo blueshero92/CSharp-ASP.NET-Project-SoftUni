579ee9c59164256cebbe4dcddc55e9ce77fc8efd
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-01-28 18:14:45 +0200

    Added views for Developers and Publishers with buttons between them and Games. Added them to the navigation bar.

--- GamingZoneApp/Controllers/DevelopersController.cs
+++ GamingZoneApp/Controllers/DevelopersController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GamingZoneApp.Data;
+using GamingZoneApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamingZoneApp.Controllers
 {
     public class DevelopersController : Controller
     {
+        private readonly GamingZoneDbContext dbContext;
+
+        public DevelopersController(GamingZoneDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Developer> developers = dbContext
+                                             .Developers
+                                             .Include(d => d.GamesDeveloped)
+                                             .OrderBy(d => d.Name)
+                                             .AsNoTracking()
+                                             .ToList();
+
+            return View(developers);
+        }
+
+        //Visualize all games by a specific developer.
+        //Created buttons to be able to access this view from the Developers/Index view.
+        public IActionResult DeveloperGames(Guid developerId)
+        {
+            IEnumerable<Game> gamesByDev = dbContext
+                                       .Games
+                                       .Include(g => g.Developer)
+                                       .Include(g => g.Publisher)
+                                       .Where(g => g.DeveloperId == developerId)
+                                       .AsNoTracking()
+                                       .AsSplitQuery()
+                                       .ToList();
+
+            return View(gamesByDev);
+
         }
     }
 }
--- GamingZoneApp/Controllers/PublishersController.cs
+++ GamingZoneApp/Controllers/PublishersController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GamingZoneApp.Data;
+using GamingZoneApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamingZoneApp.Controllers
 {
     public class PublishersController : Controller
     {
+        private readonly GamingZoneDbContext dbContext;
+
+        public PublishersController(GamingZoneDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Publisher> publishers = dbContext
+                                             .Publishers
+                                             .Include(p => p.GamesPublished)
+                                             .OrderBy(p => p.Name)
+                                             .AsNoTracking()
+                                             .ToList();
+
+            return View(publishers);
+        }
+
+        //Visualize all games by a specific publisherr.
+        //Created buttons to be able to access this view from the Publishers/Index view.
+        public IActionResult PublisherGames(Guid publisherId)
+        {
+            IEnumerable<Game> gamesByPublisher = dbContext
+                                       .Games
+                                       .Include(g => g.Developer)
+                                       .Include(g => g.Publisher)
+                                       .Where(g => g.PublisherId == publisherId)
+                                       .AsNoTracking()
+                                       .AsSplitQuery()
+                                       .ToList();
+            
+            return View(gamesByPublisher);
         }
     }
 }
