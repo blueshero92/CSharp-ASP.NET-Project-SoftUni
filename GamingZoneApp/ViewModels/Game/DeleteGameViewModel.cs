86d31ab04ecbafe824cff494a9d51e60e700d9a3
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-02-03 22:11:31 +0200

    Implemmented "Delete" feature for games.

---
+++ GamingZoneApp/ViewModels/Game/DeleteGameViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GamingZoneApp.ViewModels.Game
+{
+    // View model for deleting a game.
+    public class DeleteGameViewModel
+    {
+        public string? Title { get; set; }
+    }
+}
