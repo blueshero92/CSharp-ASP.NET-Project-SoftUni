0baf05ba5f8f2f36589538b9a7902dbb2b375510
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-03-20 18:41:22 +0200

    Introduced Admin area.

--- GamingZoneApp/Program.cs
+++ GamingZoneApp/Program.cs
@@ -93,6 +93,10 @@ namespace GamingZoneApp
             app.UseStatusCodePagesWithRedirects("/Home/Error/{0}");
 
             app.MapControllerRoute(
+                name: "adminArea",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
