6034293f6fc4c4a6426a1adce3a4563b172857c4
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-04-01 16:56:23 +0300

    Implemented pagination for Developers and Publishers.

--- GamingZoneApp/Controllers/DevelopersController.cs
+++ GamingZoneApp/Controllers/DevelopersController.cs
@@ -1,6 +1,9 @@
 ﻿using GamingZoneApp.Services.Core.Interfaces;
 using GamingZoneApp.ViewModels.Developer;
 using GamingZoneApp.ViewModels.Game;
+using GamingZoneApp.GCommon.Pagination;
+using static GamingZoneApp.GCommon.Constants.AppConstants;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,23 +23,31 @@ namespace GamingZoneApp.Controllers
         //Visualize all developers using view model.
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? pageNumber)
         {
-            IEnumerable<AllDevelopersViewModel> developersViewModel = await developerService.GetAllDevelopersWithInfoAsync();                                                              
+            IEnumerable<AllDevelopersViewModel> developersViewModel = await developerService.GetAllDevelopersWithInfoAsync();
 
-            return View(developersViewModel);
+            //Size of the page for pagination.
+            int pageSize = PageSize;
+
+            //Using the PaginatedList class to create a paginated list of developers and passing it to the view.
+            return View(await PaginatedList<AllDevelopersViewModel>.CreateAsync(developersViewModel, pageNumber ?? 1, pageSize));
         }
 
         //Visualize all games by a specific developer using a view model.
         //Created buttons to be able to access this view from the Developers/Index view.
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> DeveloperGames(Guid developerId)
+        public async Task<IActionResult> DeveloperGames(Guid developerId, int? pageNumber)
         {
-
+            //Getting all games by a specific developer using the developer service and passing it to the view model.
             IEnumerable<AllGamesViewModel> gamesByDevViewModel = await developerService.GetAllGamesByDeveloperIdAsync(developerId);
-                
-            return View(gamesByDevViewModel);
+
+            //Size of the page for pagination.
+            int pageSize = PageSize;
+
+            //Using the PaginatedList class to create a paginated list of games and passing it to the view.
+            return View(await PaginatedList<AllGamesViewModel>.CreateAsync(gamesByDevViewModel, pageNumber ?? 1, pageSize));
 
         }
     }
--- GamingZoneApp/Controllers/PublishersController.cs
+++ GamingZoneApp/Controllers/PublishersController.cs
@@ -1,6 +1,10 @@
 ﻿using GamingZoneApp.Services.Core.Interfaces;
 using GamingZoneApp.ViewModels.Game;
 using GamingZoneApp.ViewModels.Publisher;
+using GamingZoneApp.GCommon.Pagination;
+
+using static GamingZoneApp.GCommon.Constants.AppConstants;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,24 +22,32 @@ namespace GamingZoneApp.Controllers
         //Visualize all publishers using a view model.
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? pageNumber)
         {
             //Get all publishers with their info using the service and map them to the view model.
             IEnumerable<AllPublishersViewModel> publishersViewModel = await publisherService.GetAllPublishersWithInfoAsync();
-                
-            return View(publishersViewModel);
+
+            //Size of the page for pagination.
+            int pageSize = PageSize;
+
+            //Return the view with the paginated list of publishers.
+            return View(await PaginatedList<AllPublishersViewModel>.CreateAsync(publishersViewModel, pageNumber ?? 1, pageSize));
         }
 
         //Visualize all games by a specific publisher using a view model.
         //Created buttons to be able to access this view from the Publishers/Index view.
         [HttpGet]
         [AllowAnonymous]
-        public async  Task<IActionResult> PublisherGames(Guid publisherId)
+        public async  Task<IActionResult> PublisherGames(Guid publisherId, int? pageNumber)
         {
             //Get all games by the publisher's id using the service and map them to the view model.
             IEnumerable<AllGamesViewModel> gamesByPublisherViewModel = await publisherService.GetAllGamesByPublisherIdAsync(publisherId);
 
-            return View(gamesByPublisherViewModel);
+            //Size of the page for pagination.
+            int pageSize = PageSize;
+
+            //Return the view with the paginated list of games by the publisher.
+            return View(await PaginatedList<AllGamesViewModel>.CreateAsync(gamesByPublisherViewModel, pageNumber ?? 1, pageSize));
         }
     }
 }
