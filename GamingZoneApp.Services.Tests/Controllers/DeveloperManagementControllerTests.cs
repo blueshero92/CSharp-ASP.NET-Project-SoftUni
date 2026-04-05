644d9c81d4084fa880d184438d7702f8540674fe
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-04-05 14:39:12 +0300

    Added unit tests for Developer and Publisher management controllers.

---
+++ GamingZoneApp.Services.Tests/Controllers/DeveloperManagementControllerTests.cs
@@ -0,0 +1,314 @@
+﻿using GamingZoneApp.Areas.Admin.Controllers;
+using GamingZoneApp.Services.Core.Interfaces;
+using GamingZoneApp.ViewModels.Admin.Developer;
+using GamingZoneApp.ViewModels.Developer;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+using Moq;
+using NUnit.Framework;
+
+using System.Security.Claims;
+
+namespace GamingZoneApp.Services.Tests.Controllers
+{
+    [TestFixture]
+    public class DeveloperManagementControllerTests
+    {
+        private Mock<IDeveloperService> developerServiceMock;
+        private Mock<IDeveloperManagementService> developerManagementServiceMock;
+        private DeveloperManagementController controller;
+
+        private static readonly Guid TestDeveloperId = Guid.NewGuid();
+
+        [SetUp]
+        public void SetUp()
+        {
+            developerServiceMock = new Mock<IDeveloperService>();
+            developerManagementServiceMock = new Mock<IDeveloperManagementService>();
+
+            controller = new DeveloperManagementController(
+                developerServiceMock.Object,
+                developerManagementServiceMock.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.Role, "Admin")
+                    }, "TestAuth"))
+                }
+            };
+
+            controller.TempData = new TempDataDictionary(
+                controller.HttpContext,
+                Mock.Of<ITempDataProvider>());
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewWithAllDevelopers()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.GetAllDevelopersWithInfoAsync())
+                                .ReturnsAsync(new List<AllDevelopersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+            developerServiceMock.Verify(ds => ds.GetAllDevelopersWithInfoAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task AddDeveloper_Get_ReturnsView()
+        {
+            // Arrange
+            developerManagementServiceMock.Setup(dms => dms.AddDeveloperAsync(It.IsAny<DeveloperInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.AddDeveloper();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddDeveloper_Post_InvalidModel_ReturnsView()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Name", "Required");
+
+            // Act
+            IActionResult result = await controller.AddDeveloper(new DeveloperInputModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddDeveloper_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            developerManagementServiceMock.Setup(dms => dms.AddDeveloperAsync(It.IsAny<DeveloperInputModel>()))
+                                          .ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.AddDeveloper(new DeveloperInputModel { Name = "TestDev", Description = "Desc" });
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddDeveloper_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            developerManagementServiceMock.Setup(dms => dms.AddDeveloperAsync(It.IsAny<DeveloperInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.AddDeveloper(new DeveloperInputModel { Name = "TestDev", Description = "Desc" });
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task EditDeveloper_Get_DeveloperDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditDeveloper_Get_ServiceReturnsNull_RedirectsToIndex()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.GetDeveloperForEditAsync(TestDeveloperId))
+                                          .ReturnsAsync((DeveloperInputModel?)null);
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId);
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task EditDeveloper_Get_Success_ReturnsView()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.GetDeveloperForEditAsync(TestDeveloperId))
+                                          .ReturnsAsync(new DeveloperInputModel { Name = "TestDev" });
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(((DeveloperInputModel)viewResult!.Model!).Name, Is.EqualTo("TestDev"));
+        }
+
+        [Test]
+        public async Task EditDeveloper_Post_DeveloperDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId, new DeveloperInputModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditDeveloper_Post_InvalidModel_ReturnsView()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            controller.ModelState.AddModelError("Name", "Required");
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId, new DeveloperInputModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task EditDeveloper_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.EditDeveloperAsync(TestDeveloperId, It.IsAny<DeveloperInputModel>()))
+                                          .ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId, new DeveloperInputModel { Name = "TestDev", Description = "Desc" });
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task EditDeveloper_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.EditDeveloperAsync(TestDeveloperId, It.IsAny<DeveloperInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.EditDeveloper(TestDeveloperId, new DeveloperInputModel { Name = "TestDev", Description = "Desc" });
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Get_DeveloperDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Get_ServiceReturnsNull_RedirectsToIndex()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.GetDeveloperForDeleteAsync(TestDeveloperId))
+                                          .ReturnsAsync((DeleteDeveloperViewModel?)null);
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId);
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Get_Success_ReturnsView()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.GetDeveloperForDeleteAsync(TestDeveloperId))
+                                          .ReturnsAsync(new DeleteDeveloperViewModel { Name = "TestDev" });
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Post_DeveloperDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId, new DeleteDeveloperViewModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.HardDeleteDeveloperAsync(TestDeveloperId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId, new DeleteDeveloperViewModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeleteDeveloper_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.DeveloperExistsAsync(TestDeveloperId)).ReturnsAsync(true);
+            developerManagementServiceMock.Setup(dms => dms.HardDeleteDeveloperAsync(TestDeveloperId)).ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.DeleteDeveloper(TestDeveloperId, new DeleteDeveloperViewModel());
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+    }
+}
---
+++ GamingZoneApp.Services.Tests/Controllers/PublisherManagementControllerTests.cs
@@ -0,0 +1,299 @@
+﻿using GamingZoneApp.Areas.Admin.Controllers;
+using GamingZoneApp.Services.Core.Interfaces;
+using GamingZoneApp.ViewModels.Admin.Publisher;
+using GamingZoneApp.ViewModels.Publisher;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+using Moq;
+using NUnit.Framework;
+using System.Security.Claims;
+
+namespace GamingZoneApp.Services.Tests.Controllers
+{
+    [TestFixture]
+    public class PublisherManagementControllerTests
+    {
+        private Mock<IPublisherService> publisherServiceMock;
+        private Mock<IPublisherManagementService> publisherManagementServiceMock;
+        private PublisherManagementController controller;
+
+        private static readonly Guid TestPublisherId = Guid.NewGuid();
+
+        [SetUp]
+        public void SetUp()
+        {
+            publisherServiceMock = new Mock<IPublisherService>();
+            publisherManagementServiceMock = new Mock<IPublisherManagementService>();
+
+            controller = new PublisherManagementController(
+                publisherServiceMock.Object,
+                publisherManagementServiceMock.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.Role, "Admin")
+                    }, "TestAuth"))
+                }
+            };
+
+            controller.TempData = new TempDataDictionary(
+                controller.HttpContext,
+                Mock.Of<ITempDataProvider>());
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewWithAllPublishers()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.GetAllPublishersWithInfoAsync())
+                                .ReturnsAsync(new List<AllPublishersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+            publisherServiceMock.Verify(ps => ps.GetAllPublishersWithInfoAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task AddPublisher_Get_ReturnsView()
+        {
+            // Arrange
+            publisherManagementServiceMock.Setup(pms => pms.AddPublisherAsync(It.IsAny<PublisherInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.AddPublisher();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddPublisher_Post_InvalidModel_ReturnsView()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Name", "Required");
+
+            // Act
+            IActionResult result = await controller.AddPublisher(new PublisherInputModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddPublisher_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            publisherManagementServiceMock.Setup(pms => pms.AddPublisherAsync(It.IsAny<PublisherInputModel>()))
+                                          .ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.AddPublisher(new PublisherInputModel { Name = "TestPub", Description = "Desc" });
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task AddPublisher_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            publisherManagementServiceMock.Setup(pms => pms.AddPublisherAsync(It.IsAny<PublisherInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.AddPublisher(new PublisherInputModel { Name = "TestPub", Description = "Desc" });
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task EditPublisher_Get_PublisherDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditPublisher_Get_ServiceReturnsNull_RedirectsToIndex()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.GetPublisherForEditAsync(TestPublisherId))
+                                          .ReturnsAsync((PublisherInputModel?)null);
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId);
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task EditPublisher_Get_Success_ReturnsView()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.GetPublisherForEditAsync(TestPublisherId))
+                                          .ReturnsAsync(new PublisherInputModel { Name = "TestPub" });
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(((PublisherInputModel)viewResult!.Model!).Name, Is.EqualTo("TestPub"));
+        }
+
+        [Test]
+        public async Task EditPublisher_Post_PublisherDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId, new PublisherInputModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditPublisher_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.EditPublisherAsync(TestPublisherId, It.IsAny<PublisherInputModel>()))
+                                          .ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId, new PublisherInputModel { Name = "TestPub", Description = "Desc" });
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task EditPublisher_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.EditPublisherAsync(TestPublisherId, It.IsAny<PublisherInputModel>()))
+                                          .ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.EditPublisher(TestPublisherId, new PublisherInputModel { Name = "TestPub", Description = "Desc" });
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task DeletePublisher_Get_PublisherDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task DeletePublisher_Get_ServiceReturnsNull_RedirectsToIndex()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.GetPublisherForDeleteAsync(TestPublisherId))
+                                          .ReturnsAsync((DeletePublisherViewModel?)null);
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId);
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task DeletePublisher_Get_Success_ReturnsView()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.GetPublisherForDeleteAsync(TestPublisherId))
+                                          .ReturnsAsync(new DeletePublisherViewModel { Name = "TestPub" });
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeletePublisher_Post_PublisherDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId, new DeletePublisherViewModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task DeletePublisher_Post_ServiceFails_ReturnsView()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.HardDeletePublisherAsync(TestPublisherId)).ReturnsAsync(false);
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId, new DeletePublisherViewModel());
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeletePublisher_Post_Success_RedirectsToIndex()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.PublisherExistsAsync(TestPublisherId)).ReturnsAsync(true);
+            publisherManagementServiceMock.Setup(pms => pms.HardDeletePublisherAsync(TestPublisherId)).ReturnsAsync(true);
+
+            // Act
+            IActionResult result = await controller.DeletePublisher(TestPublisherId, new DeletePublisherViewModel());
+
+            // Assert
+            RedirectToActionResult redirect = result as RedirectToActionResult;
+            Assert.That(redirect!.ActionName, Is.EqualTo("Index"));
+        }
+    }
+}
