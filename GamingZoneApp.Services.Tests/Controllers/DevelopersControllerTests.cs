98cce0649644cd3cd797f3f2f60c6925f6a45d47
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-04-05 13:07:43 +0300

    Added unit tests for Developer and Publisher controllers.

---
+++ GamingZoneApp.Services.Tests/Controllers/DevelopersControllerTests.cs
@@ -0,0 +1,143 @@
+﻿using GamingZoneApp.Controllers;
+using GamingZoneApp.Services.Core.Interfaces;
+using GamingZoneApp.ViewModels.Developer;
+using GamingZoneApp.ViewModels.Game;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+using NUnit.Framework;
+
+using System.Security.Claims;
+
+namespace GamingZoneApp.Services.Tests.Controllers
+{
+    [TestFixture]
+    public class DevelopersControllerTests
+    {
+        private Mock<IDeveloperService> developerServiceMock;
+        private DevelopersController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            developerServiceMock = new Mock<IDeveloperService>();
+
+            controller = new DevelopersController(developerServiceMock.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+                    }, "TestAuth"))
+                }
+            };
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewWithPaginatedDevelopers()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.GetAllDevelopersWithInfoAsync())
+                                .ReturnsAsync(new List<AllDevelopersViewModel>
+                                {
+                                    new AllDevelopersViewModel { Name = "TestDeveloperI" },
+                                    new AllDevelopersViewModel { Name = "TestDeveloperII" }
+                                });
+
+            // Act
+            IActionResult result = await controller.Index(null);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            developerServiceMock.Verify(ds => ds.GetAllDevelopersWithInfoAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task Index_NoDevelopers_ReturnsViewWithEmptyList()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.GetAllDevelopersWithInfoAsync())
+                                .ReturnsAsync(new List<AllDevelopersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index(null);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task Index_WithPageNumber_ReturnsViewResult()
+        {
+            // Arrange
+            developerServiceMock.Setup(ds => ds.GetAllDevelopersWithInfoAsync())
+                                .ReturnsAsync(new List<AllDevelopersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index(2);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeveloperGames_ReturnsViewWithPaginatedGames()
+        {
+            // Arrange
+            Guid developerId = Guid.NewGuid();
+
+            developerServiceMock.Setup(ds => ds.GetAllGamesByDeveloperIdAsync(developerId))
+                                .ReturnsAsync(new List<AllGamesViewModel>
+                                {
+                                    new AllGamesViewModel { Title = "TestGameI" },
+                                    new AllGamesViewModel { Title = "TestGameII" }
+                                });
+
+            // Act
+            IActionResult result = await controller.DeveloperGames(developerId, null);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            developerServiceMock.Verify(ds => ds.GetAllGamesByDeveloperIdAsync(developerId), Times.Once);
+        }
+
+        [Test]
+        public async Task DeveloperGames_NoGames_ReturnsViewWithEmptyList()
+        {
+            // Arrange
+            Guid developerId = Guid.NewGuid();
+
+            developerServiceMock.Setup(ds => ds.GetAllGamesByDeveloperIdAsync(developerId))
+                                .ReturnsAsync(new List<AllGamesViewModel>());
+
+            // Act
+            IActionResult result = await controller.DeveloperGames(developerId, null);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task DeveloperGames_WithPageNumber_ReturnsViewResult()
+        {
+            // Arrange
+            Guid developerId = Guid.NewGuid();
+
+            developerServiceMock.Setup(ds => ds.GetAllGamesByDeveloperIdAsync(developerId))
+                                .ReturnsAsync(new List<AllGamesViewModel>());
+
+            // Act
+            IActionResult result = await controller.DeveloperGames(developerId, 2);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+    }
+}
---
+++ GamingZoneApp.Services.Tests/Controllers/PublishersControllerTests.cs
@@ -0,0 +1,143 @@
+﻿using GamingZoneApp.Controllers;
+using GamingZoneApp.Services.Core.Interfaces;
+using GamingZoneApp.ViewModels.Game;
+using GamingZoneApp.ViewModels.Publisher;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+using NUnit.Framework;
+
+using System.Security.Claims;
+
+namespace GamingZoneApp.Services.Tests.Controllers
+{
+    [TestFixture]
+    public class PublishersControllerTests
+    {
+        private Mock<IPublisherService> publisherServiceMock;
+        private PublishersController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            publisherServiceMock = new Mock<IPublisherService>();
+
+            controller = new PublishersController(publisherServiceMock.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+                    }, "TestAuth"))
+                }
+            };
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewWithPaginatedPublishers()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.GetAllPublishersWithInfoAsync())
+                                .ReturnsAsync(new List<AllPublishersViewModel>
+                                {
+                                    new AllPublishersViewModel { Name = "TestPublisherI" },
+                                    new AllPublishersViewModel { Name = "TestPublisherII" }
+                                });
+
+            // Act
+            IActionResult result = await controller.Index(null);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            publisherServiceMock.Verify(ps => ps.GetAllPublishersWithInfoAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task Index_NoPublishers_ReturnsViewWithEmptyList()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.GetAllPublishersWithInfoAsync())
+                                .ReturnsAsync(new List<AllPublishersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index(null);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task Index_WithPageNumber_ReturnsViewResult()
+        {
+            // Arrange
+            publisherServiceMock.Setup(ps => ps.GetAllPublishersWithInfoAsync())
+                                .ReturnsAsync(new List<AllPublishersViewModel>());
+
+            // Act
+            IActionResult result = await controller.Index(2);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task PublisherGames_ReturnsViewWithPaginatedGames()
+        {
+            // Arrange
+            Guid publisherId = Guid.NewGuid();
+
+            publisherServiceMock.Setup(ps => ps.GetAllGamesByPublisherIdAsync(publisherId))
+                                .ReturnsAsync(new List<AllGamesViewModel>
+                                {
+                                    new AllGamesViewModel { Title = "TestGameI" },
+                                    new AllGamesViewModel { Title = "TestGameII" }
+                                });
+
+            // Act
+            IActionResult result = await controller.PublisherGames(publisherId, null);
+
+            // Assert
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            publisherServiceMock.Verify(ps => ps.GetAllGamesByPublisherIdAsync(publisherId), Times.Once);
+        }
+
+        [Test]
+        public async Task PublisherGames_NoGames_ReturnsViewWithEmptyList()
+        {
+            // Arrange
+            Guid publisherId = Guid.NewGuid();
+
+            publisherServiceMock.Setup(ps => ps.GetAllGamesByPublisherIdAsync(publisherId))
+                                .ReturnsAsync(new List<AllGamesViewModel>());
+
+            // Act
+            IActionResult result = await controller.PublisherGames(publisherId, null);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task PublisherGames_WithPageNumber_ReturnsViewResult()
+        {
+            // Arrange
+            Guid publisherId = Guid.NewGuid();
+
+            publisherServiceMock.Setup(ps => ps.GetAllGamesByPublisherIdAsync(publisherId))
+                                .ReturnsAsync(new List<AllGamesViewModel>());
+
+            // Act
+            IActionResult result = await controller.PublisherGames(publisherId, 2);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+    }
+}
